5855eca4acb27da63c6ea9534c6f9ac05909f22d
Maevrim <maevrim@gmail.com>
2017-02-10 20:18:58 -0500

    first

---
+++ README.md
@@ -0,0 +1,48 @@
+Collaborative Conan Map v.1
+Aimee Jarboe
+https://twitter.com/ProbablyAimee
+++++++++++++++++++++++++++++++++
+
+Requirements:
+
+*Server with LAMP stack
+*make sure php-dom is installed and enabled
+*enabled Google Maps API key ("Get a Key" button at https://developers.google.com/maps/documentation/embed/ )
+
+
+1) Create table in mysql database:
+
+ CREATE TABLE IF NOT EXISTS `markers` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `name` varchar(60) NOT NULL,
+   `address` varchar(80) NOT NULL,
+   `lat` float(10,6) NOT NULL,
+   `lng` float(10,6) NOT NULL,
+   `type` varchar(30) NOT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+2) In Code folder, open index.php and replace following values:
+
+ "YOUR_API_KEY" with your Google Maps API key in URL on line 6
+
+ "---YOUR DOMAIN HERE---" in icon references with your domain.
+
+3) In Code folder, open map_process.php and fill in the following values:
+
+ // database settings 
+ $db_username = '';
+ $db_password = '';
+ $db_name = 'mysql';
+ $db_host = 'localhost';
+
+
+4) Upload contents of Code folder to server.
+
+++++++++++++++++++++++++++++
+
+Troubleshooting:
+
+*If results "Could not create marker!", check all columns in db table were created.
+ 
---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>NUTZ Conan Exiles Map</title>
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+
+	var map;
+
+		var repeatOnXAxis = false; // Do we need to repeat the image on the X-axis? Most likely you'll want to set this to false
+	/*
+	 * Helper function which normalizes the coords so that tiles can repeat across the X-axis (horizontally) like the standard Google map tiles.
+	 * ----------------
+	 */
+
+		function getNormalizedCoord(coord, zoom) {
+			if (!repeatOnXAxis) return coord;
+
+			var y = coord.y;
+			var x = coord.x;
+
+			// tile range in one direction range is dependent on zoom level
+			// 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+			var tileRange = 1 << zoom;
+
+			// don't repeat across Y-axis (vertically)
+			if (y < 0 || y >= tileRange) {
+				return null;
+			}
+
+			// repeat across X-axis
+			if (x < 0 || x >= tileRange) {
+				x = (x % tileRange + tileRange) % tileRange;
+			}
+
+			return {
+				x: x,
+				y: y
+			};
+
+		}
+
+	map_initialize(); // initialize google map
+	
+	//############### Google Map Initialize ##############
+	function map_initialize()
+	{
+
+		// Define our custom map type
+			var customMapType = new google.maps.ImageMapType({
+				getTileUrl: function(coord, zoom) {
+					var normalizedCoord = getNormalizedCoord(coord, zoom);
+					if(normalizedCoord && (normalizedCoord.x < Math.pow(2, zoom)) && (normalizedCoord.x > -1) && (normalizedCoord.y < Math.pow(2, zoom)) && (normalizedCoord.y > -1)) {
+						return zoom + '_' + normalizedCoord.x + '_' + normalizedCoord.y + '.jpg';
+					} else {
+						return 'empty.jpg';
+					}
+				}, 
+				tileSize: new google.maps.Size(256, 256),
+				maxZoom: 4,
+				name: 'PS_Bramus.GoogleMapsTileCutter'
+			});
+
+			var googleMapOptions = 
+			{ 
+				center: new google.maps.LatLng(0, 0), // map center
+				zoom: 2, //zoom level, 0 = earth view to higher value
+				maxZoom: 4,
+				minZoom: 0,
+				zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.SMALL //zoom control size
+			},
+				streetViewControl: false,
+				mapTypeControl: false,
+				scaleControl: false, // enable scale control
+				mapTypeId: 'custom' // google map type
+			};
+		
+		   	map = new google.maps.Map(document.getElementById("google_map"), googleMapOptions);
+		   	map.mapTypes.set('custom', customMapType);	
+		   	map.setMapTypeId('custom');	
+			
+			//Load Markers from the XML File, Check (map_process.php)
+			$.get("map_process.php", function (data) {
+				$(data).find("marker").each(function () {
+					  var name 		= $(this).attr('name');
+					  var address 	= '<p>'+ $(this).attr('address') +'</p>';
+					  var type 		= $(this).attr('type');
+					  var point 	= new google.maps.LatLng(parseFloat($(this).attr('lat')),parseFloat($(this).attr('lng')));
+					  create_marker(point, name, address, false, false, false, "http://--YOUR DOMAIN HERE--/images/icons/pin_blue.png");
+				});
+			});	
+			
+			//Right Click to Drop a New Marker
+			google.maps.event.addListener(map, 'rightclick', function(event) {
+				//Edit form to be displayed with new marker
+				var EditForm = '<p><div class="marker-edit">'+
+				'<form action="ajax-save.php" method="POST" name="SaveMarker" id="SaveMarker">'+
+				'<label for="pName"><span>Place Name :</span><input type="text" name="pName" class="save-name" placeholder="Enter Title" maxlength="40" /></label>'+
+				'<label for="pDesc"><span>Description :</span><textarea name="pDesc" class="save-desc" placeholder="Enter Address" maxlength="150"></textarea></label>'+
+				'<label for="pType"><span>Type :</span> <select name="pType" class="save-type"><option value="camp">Camp</option><option value="resource">Resource</option>'+
+				//add new types in the dropdown here
+				'<option value="base">Base</option><option value="enemy">Enemy</option><option value="NPC">NPC Friendly</option><option value="landmark">Landmark</option></select></label>'+
+				'</form>'+
+				'</div></p><button name="save-marker" class="save-marker">Save Marker Details</button>';
+
+				//Drop a new Marker with our Edit Form
+				create_marker(event.latLng, 'New Marker', EditForm, true, true, true, "http://---YOUR DOMAIN HERE---/images/icons/pin_green.png");
+			});
+										
+	}
+	
+	//############### Create Marker Function ##############
+	function create_marker(MapPos, MapTitle, MapDesc,  InfoOpenDefault, DragAble, Removable, iconPath)
+	{	  	  		  
+		
+		//new marker
+		var marker = new google.maps.Marker({
+			position: MapPos,
+			map: map,
+			draggable:DragAble,
+			animation: google.maps.Animation.DROP,
+			title: MapTitle,
+			icon: iconPath
+		});
+		
+		//Content structure of info Window for the Markers
+		var contentString = $('<div class="marker-info-win">'+
+		'<div class="marker-inner-win"><span class="info-content">'+
+		'<h1 class="marker-heading">'+MapTitle+'</h1>'+
+		MapDesc+ 
+		'</span><button name="remove-marker" class="remove-marker" title="Remove Marker">Remove Marker</button>'+
+		'</div></div>');	
+
+		
+		//Create an infoWindow
+		var infowindow = new google.maps.InfoWindow();
+		//set the content of infoWindow
+		infowindow.setContent(contentString[0]);
+
+		//Find remove button in infoWindow
+		var removeBtn 	= contentString.find('button.remove-marker')[0];
+		var saveBtn 	= contentString.find('button.save-marker')[0];
+
+		//add click listner to remove marker button
+		google.maps.event.addDomListener(removeBtn, "click", function(event) {
+			remove_marker(marker);
+		});
+		
+		if(typeof saveBtn !== 'undefined') //continue only when save button is present
+		{
+			//add click listner to save marker button
+			google.maps.event.addDomListener(saveBtn, "click", function(event) {
+				var mReplace = contentString.find('span.info-content'); //html to be replaced after success
+				var mName = contentString.find('input.save-name')[0].value; //name input field value
+				var mDesc  = contentString.find('textarea.save-desc')[0].value; //description input field value
+				var mType = contentString.find('select.save-type')[0].value; //type of marker
+				
+				if(mName =='' || mDesc =='')
+				{
+					alert("Please enter Name and Description!");
+				}else{
+					save_marker(marker, mName, mDesc, mType, mReplace); //call save marker function
+				}
+			});
+		}
+		
+		//add click listner to save marker button		 
+		google.maps.event.addListener(marker, 'click', function() {
+				infowindow.open(map,marker); // click on marker opens info window 
+	    });
+		  
+		if(InfoOpenDefault) //whether info window should be open by default
+		{
+		  infowindow.open(map,marker);
+		}
+	}
+	
+	//############### Remove Marker Function ##############
+	function remove_marker(Marker)
+	{
+		
+		/* determine whether marker is draggable 
+		new markers are draggable and saved markers are fixed */
+		if(Marker.getDraggable()) 
+		{
+			Marker.setMap(null); //just remove new marker
+		}
+		else
+		{
+			//Remove saved marker from DB and map using jQuery Ajax
+			var mLatLang = Marker.getPosition().toUrlValue(); //get marker position
+			var myData = {del : 'true', latlang : mLatLang}; //post variables
+			$.ajax({
+			  type: "POST",
+			  url: "map_process.php",
+			  data: myData,
+			  success:function(data){
+					Marker.setMap(null); 
+					alert(data);
+				},
+				error:function (xhr, ajaxOptions, thrownError){
+					alert(thrownError); //throw any errors
+				}
+			});
+		}
+
+	}
+	
+	//############### Save Marker Function ##############
+	function save_marker(Marker, mName, mAddress, mType, replaceWin)
+	{
+		//Save new marker using jQuery Ajax
+		var mLatLang = Marker.getPosition().toUrlValue(); //get marker position
+		var myData = {name : mName, address : mAddress, latlang : mLatLang, type : mType }; //post variables
+		console.log(replaceWin);		
+		$.ajax({
+		  type: "POST",
+		  url: "map_process.php",
+		  data: myData,
+		  success:function(data){
+				replaceWin.html(data); //replace info window with new html
+				Marker.setDraggable(false); //set marker to fixed
+				Marker.setIcon('http://---YOUR DOMAIN HERE---/images/icons/pin_blue.png'); //replace icon
+            },
+            error:function (xhr, ajaxOptions, thrownError){
+                alert(thrownError); //throw any errors
+            }
+		});
+	}
+
+});
+</script>
+
+<style type="text/css">
+h1.heading{padding:0px;margin: 0px 0px 10px 0px;text-align:center;font: 18px Georgia, "Times New Roman", Times, serif;}
+
+/* width and height of google map */
+#google_map {width: 90%; height: 800px;margin-top:0px;margin-left:auto;margin-right:auto;}
+
+/* Marker Edit form */
+.marker-edit label{display:block;margin-bottom: 5px;}
+.marker-edit label span {width: 100px;float: left;}
+.marker-edit label input, .marker-edit label select{height: 24px;}
+.marker-edit label textarea{height: 60px;}
+.marker-edit label input, .marker-edit label select, .marker-edit label textarea {width: 60%;margin:0px;padding-left: 5px;border: 1px solid #DDD;border-radius: 3px;}
+
+/* Marker Info Window */
+h1.marker-heading{color: #585858;margin: 0px;padding: 0px;font: 18px "Trebuchet MS", Arial;border-bottom: 1px dotted #D8D8D8;}
+div.marker-info-win {max-width: 500px;}
+div.marker-info-win p{padding: 0px;margin: 10px 0px 10px 0;}
+div.marker-inner-win{padding: 5px;}
+button.save-marker, button.remove-marker{border: none;background: rgba(0, 0, 0, 0);color: #00F;padding: 0px;text-decoration: underline;margin-right: 10px;cursor: pointer;
+}
+</style>
+</head>
+<body>             
+<h1 class="heading">NUTZ Map</h1>
+<div align="center">Right Click to Drop a New Marker</div>
+<div id="google_map"></div>
+</body>
+</html>
